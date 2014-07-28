d49a6a07d248d056e5150df6aff0ec0ef0feecd7
Cesar Zavala <cesarzavalamesta@g.harvard.edu>
2014-07-28 01:14:02 -0400

    Added ability to open files from any image file type

--- app/models/Mingle.php
+++ app/models/Mingle.php
@@ -25,10 +25,35 @@ class Mingle extends Eloquent {
 		$this->picture = $thepicture;
 	}
 
+//http://stackoverflow.com/questions/10233577/create-image-from-url-any-file-type
+	private function imagecreatefromfile( $filename ) {
+    if (!file_exists($filename)) {
+        throw new InvalidArgumentException('File "'.$filename.'" not found.');
+    }
+    switch ( strtolower( pathinfo( $filename, PATHINFO_EXTENSION ))) {
+        case 'jpeg':
+        case 'jpg':
+            return imagecreatefromjpeg($filename);
+        break;
+
+        case 'png':
+            return imagecreatefrompng($filename);
+        break;
+
+        case 'gif':
+            return imagecreatefromgif($filename);
+        break;
+
+        default:
+            throw new InvalidArgumentException('File "'.$filename.'" is not valid jpg, png or gif image.');
+        break;
+    }
+}
+
 	public function do_mingle($filename) {
 
 			$dest = imagecreatefrompng($this->template->image);
-			$src = imagecreatefrompng($this->picture->image);
+			$src = $this->imagecreatefromfile($this->picture->image);
 
 			$size = getimagesize($this->picture->image);
 
